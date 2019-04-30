a167560091eb40dfcd89f13036c95c363e991d9c
Your Name <you@example.com>
2019-04-30 11:28:50 +0800

    整合路由以及首页

--- src/main.js
+++ src/main.js
@@ -4,8 +4,30 @@ import App from './App.vue'
 // 引入头部尾部的全局样式
 import baseCSS from './assets/statics/site/css/style.css'
 
+//导入首页组件
+import index from './components/index.vue'
+
+
+//导入路由
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+//写路由规则
+
+const routes= [
+  //首页规则
+  {path:'/index',component:index}
+]
+
+//创建路由对象
+const router = new VueRouter({
+  routes
+})
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  // 挂载
+  router
 }).$mount('#app')
